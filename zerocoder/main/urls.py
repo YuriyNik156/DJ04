69b19b677170eb7561db530efe0620198d2e034f
YuriyNik156 <yuriy.nik156@gmail.com>
2025-06-27 15:56:36 +0300

    Update urls.py

--- zerocoder/main/urls.py
+++ zerocoder/main/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . import views
+from news import views as news_views
 
 urlpatterns = [
     path('', views.index, name="home"),
-    path('create_news', views.create_news, name="add_news"),
+    path('create_news', news_views.create_news, name="add_news"),
 ]
